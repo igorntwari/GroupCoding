e63dc8d39431479eb72b4b8a72de657a094ea5ea
Gambira <user@LAPTOP-G3JCD04>
2025-08-21 16:08:00 +0200

    merging step0 and step1

--- Group3/weatherAnalytics.js
+++ Group3/weatherAnalytics.js
@@ -1,3 +1,4 @@
+
 class WeatherReading {
   constructor(id, location, temperature, humidity, pressure, windSpeed, timestamp = Date.now()) {
     this.id = id;
@@ -22,6 +23,25 @@ class WeatherReading {
                  Pressure: ${this.pressure} hPa
                  Wind Speed: ${this.windSpeed} km/h
                  Timestamp: ${new Date(this.timestamp).toLocaleString()}`;
+  }
+
+  get comfortIndex(){
+    try{
+        if(this.temperature <100) return ("Temperature validation working");
+        
+      } catch(e){
+        throw new Error("Temperature must less than 100")
+      }
+  }
+  get windCategory(){
+    this.windSpeed > 10 ? "strong": "moderate"
+  }
+  isExtremeWeather = () =>{
+    if(this.temperature < 50 && this.temperature > 20 && this.windSpeed < 10 && this.humidity > 50 && pressure > 100){
+      return true
+    }else{
+      return false
+    }
   }
 }
 
