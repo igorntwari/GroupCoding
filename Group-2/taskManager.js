3863b6c4cd9dc45170c3fc76f2b24860c8476af2
dieuaime-shami <shamidieuaime272@gmail.com>
2025-08-21 16:11:52 +0200

    Task Methods with Template Literals

--- Group-2/taskManager.js
+++ Group-2/taskManager.js
@@ -6,9 +6,19 @@ class Task{
     this.status=status;
     this.priority=priority;
     this.createdAt= new Date();
-    
+  }
+
+  getDescription(){
+    return `Task #${this.id}:${this.title}
+    Description: ${this.description}
+    Status:${this.status}
+    Priority:${this.priority}`
+  }
+  updateStatus(newStatus){
+    this.status =newStatus
+  }
+  getDaysOld(){
+   return new Date() - this.createdAt;
   }
 }
-const task = new Task(1, "Complete project", "Finish the final implementation");
-console.log(task.status);
-console.log(task.createdAt instanceof Date);
+
