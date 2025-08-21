d6b343a618595f215e44a1a7616d4f2801eb3898
elieiradukunda <iradukundaelie71@gmail.com>
2025-08-21 18:04:52 +0200

    reverting changes

--- Group-4/socialAnalytics.js
+++ Group-4/socialAnalytics.js
@@ -1,65 +1,36 @@
-class Post {
-  constructor(id, userId, content, timestamp, likes, shares, comments) {
-    this.id = id;
-    this.userId = userId;
-    this.content = content;
-    this.timestamp = timestamp;
-    this.likes = likes;
-    this.shares = shares;
-    this.comments = comments;
-  }
+class Post{
+    constructor(id, userId, content, timestamp, likes, shares,comments){
+        this.id=id;
+        this.userId=userId;
+        this.content=content;
+        this.timestamp=timestamp;
+        this.likes=likes;
+        this.shares=shares;
+        this.comments=comments;
 
-  get engagementRate() {
-    const totalInteractions = this.likes + this.shares + this.comments.length;
-    return totalInteractions / 100;
-  }
 
-  static getPostsByHashtag(posts, hashtag) {
-    return posts.filter(post => post.content.includes(`#${hashtag}`));
-  }
-}
-
-class User {
-  constructor(id, username, email, followers, following, posts = []) {
-    this.id = id;
-    this._username = username;
-    this.email = email;
-    this.followers = followers;   
-    this.following = following;   
-    this.posts = posts;          
-  }
-
-  get followerRatio() {
-    if (this.followers === 0) return 0;
-    return this.following / this.followers;
-  }
-
-  set username(newName) {
-    if (!/^[a-zA-Z0-9_]+$/.test(newName)) {
-      throw new Error("Invalid username. Use only letters, numbers, or underscore.");
     }
-    this._username = newName;
-  }
 
-  get username() {
-    return this._username;
-  }
+    toString(){
+        return `this post is for ${this.userId} : "${this.content}" at ${new Date(this.timestamp)}`;
+    }
 
-  isInfluencer = () => this.followers > 10000;
+}
+class User{
+    constructor(id, username, email, followers, following, postsarray){
+        this.id=id;
+        this.username=username;
+        this.email=email;
+        this.followers=[];
+        this.following=[];
+        this.posts=[];
+        
+    }
+    addPost(content){
+        this.posts.push(content);
+        console.log(`${this.username} added a this post of "${this.content}"`);
 
-  addPost(content) {
-    const newPost = new Post(
-      this.posts.length + 1,
-      this.id,
-      content,
-      new Date(),
-      0,
-      0,
-      []
-    );
-    this.posts.push(newPost);
-    return newPost;
-  }
+    }
 }
 
 // ---------------- TEST CASES ----------------
