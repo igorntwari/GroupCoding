47f065f5878963cdb669b2728c5e6da4c79f3b77
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-21 15:34:54 +0200

    add class of product

---
+++ productManager.js
@@ -0,0 +1,13 @@
+
+
+class Product {
+    constructor(id,name,price,category,stock){
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.category = category;
+        this.stock = stock;
+        
+
+    }
+}
