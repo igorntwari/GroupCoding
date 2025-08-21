2583354d0f34a13442e066f045ecdaafe308db68
elieiradukunda <iradukundaelie71@gmail.com>
2025-08-21 17:22:56 +0200

    some changes to step 0

--- Group-4/socialAnalytics.js
+++ Group-4/socialAnalytics.js
@@ -1,32 +1,42 @@
 class Post{
-     constructor(id, userId, content, timestamp, likes, shares, comments){
-       this.id =id;
-       this.userId =userId;
-       this.content =content;
-       this.timestamp =timestamp.getHours();
-       this.likes =likes;
-       this.shares =shares;
-       this.comments =comments;
-     }
-}
+    constructor(id, userId, content, timestamp, likes, shares,comments){
+        this.id=id;
+        this.userId=userId;
+        this.content=content;
+        this.timestamp=timestamp;
+        this.likes=likes;
+        this.shares=shares;
+        this.comments=comments;
 
+
+    }
+
+    toString(){
+        return `this post is for ${this.userId} : "${this.content}" at ${new Date(this.timestamp)}`;
+    }
+
+}
 class User{
-     constructor(id, username, email, followers, following, posts){
-       this.id =id;
-       this.username =username;
-       this.email =email;
-       this.followers =followers;
-       this.following =following;
-       this.posts =posts;
-     }
-}
-User.prototype.addPost = function(content){
-    return content;
-} 
-const user = new User(1, "john_doe", "john@example.com",0,0,[]);
-const post  = new Post(1, 1, "Hello World! #FirstPost", new Date(), 0, 0, [])
-console.log(post)
+    constructor(id, username, email, followers, following, postsarray){
+        this.id=id;
+        this.username=username;
+        this.email=email;
+        this.followers=[];
+        this.following=[];
+        this.posts=[];
+        
+    }
+    addPost(content){
+        this.posts.push(content);
+        console.log(`${this.username} added a this post of "${this.content}"`);
 
+    }
+}
+
+const user = new User(1, "john_doe", "john@example.com");
+const post = new Post(1, 1, "Hello World! #FirstPost", Date.now(), 0, 0, []);
+
+user.addPost(post);
 console.log(user.posts.length); // Expected: 1
 console.log(post.content.includes("#FirstPost")); // Expected: true
 console.log(post.timestamp > 0); // Expected: true
