67c99b2ce2b5c8b4ec47e7a8dd8bfe87789eefcb
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2025-08-21 16:13:40 +0200

    add static method and arrow function

--- productManager.js
+++ productManager.js
@@ -1,13 +1,22 @@
-
-
 class Product {
-    constructor(id,name,price,category,stock){
-        this.name = name;
+    constructor(id, name, price, category, stock) {
         this.id = id;
+        this.name = name;
         this.price = price;
         this.category = category;
         this.stock = stock;
-        
+    }
 
+    static comparePrice(product1, product2) {
+        if (product1.price === product2.price) {
+            return `The Products price are equal`
+        }
+        return product1.price > product2.price ? product2 : product1;
+    }
+
+    static generateId() {
+        return Math.floor(100000 + Math.random() * 900000);
     }
 }
+
+
