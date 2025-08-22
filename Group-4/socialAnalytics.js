d0d33de194c6fb2b120c77f03a40f29a6213983a
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-22 15:17:59 +0200

    feat: step 2

--- Group-4/socialAnalytics.js
+++ Group-4/socialAnalytics.js
@@ -1,37 +1,3 @@
-class Post{
-     constructor(id, userId, content, timestamp, likes, shares, comments){
-       this.id =id;
-       this.userId =userId;
-       this.content =content;
-       this.timestamp =timestamp.getHours();
-       this.likes =likes;
-       this.shares =shares;
-       this.comments =[];
-     }
-}
-
-class User{
-     constructor(id, username, email, followers, following, posts){
-       this.id =id;
-       this.username =username;
-       this.email =email;
-       this.followers =followers;
-       this.following =following;
-       this.posts =[];
-     }
-}
-User.prototype.addPost = function(content){
-    return content;
-} 
-const user = new User(1, "john_doe", "john@example.com", 15000, 500,[200]);
-const post  = new Post(1, 1, "Hello World! #FirstPost", new Date(), 0, 0, [])
-console.log(post)
-console.log(user)
-console.log(user.posts.length); // Expected: 1
-console.log(post.content.includes("#FirstPost")); // Expected: true
-console.log(post.timestamp > 0); // Expected: true
-
-
 // STEP 2
 class  AnalyticsEngine{
 
